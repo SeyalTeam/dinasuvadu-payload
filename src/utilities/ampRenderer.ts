cee11d1c4d5179851ca9c69d7af2014c32e969cc
Hello-CMS-AI <hellocms.ai@gmail.com>
2026-06-07 18:07:28 +0530

    font preload

--- src/utilities/ampRenderer.ts
+++ src/utilities/ampRenderer.ts
@@ -291,6 +291,8 @@ export async function renderAmpPost(post: any): Promise<string> {
     <script async src="https://cdn.ampproject.org/v0.js"></script>
     
     <link rel="preconnect" href="https://media.dinasuvadu.com" crossorigin>
+    <link rel="preload" href="/fonts/mukta-malar-400-tamil.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="/fonts/mukta-malar-700-tamil.woff2" as="font" type="font/woff2" crossorigin>
     ${heroImageUrl ? `<link rel="preload" href="${heroImagePreloadUrl}" ${heroImageSrcset ? `imagesrcset="${heroImageSrcset}" imagesizes="${heroImageSizes}"` : ''} as="image" fetchpriority="high">` : ''}
     
     <title>${escapeHtml(post.title)} – Dinasuvadu</title>
